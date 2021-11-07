b0a7d1cf63b31c9bdd5303e36a0b76a63c52eb87
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2021-11-07 12:57:27 +0100

    add umami stats

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,6 +9,15 @@ export default function App({ Component, pageProps }: AppProps) {
       <Nav />
       <Component {...pageProps} />
       <Footer />
+
+      {process.env.NODE_ENV === "production" ? (
+        <script
+          async
+          defer
+          data-website-id={process.env.UMAMI_SITE_ID}
+          src={process.env.UMAMI_URL}
+        />
+      ) : null}
     </>
   );
 }
