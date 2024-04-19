0dabba0a4dd7e699f387037c0e9f2afabf7d30f0
casperiv0 <53900565+casperiv0@users.noreply.github.com>
2024-04-19 17:03:16 +0200

    fix: types

--- src/app/api/gallery/utils.ts
+++ src/app/api/gallery/utils.ts
@@ -1,5 +1,6 @@
 import { get } from "ronin";
 
+export type GetGalleryImagesQuery = Awaited<ReturnType<typeof getGalleryImagesServer>>;
 export async function getGalleryImagesServer(after?: string | null) {
   const data = await get.galleryImages({
     limitedTo: 9,
